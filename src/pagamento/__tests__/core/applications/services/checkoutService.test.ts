150f35059d3d26bcfdd6d95202c7d8ddce5fe152
Sandro <misfitssandro@gmail.com>
2023-06-27 12:14:16 -0300

    test: criando testes da implementação do serviço de pagamento

---
+++ src/pagamento/__tests__/core/applications/services/checkoutService.test.ts
@@ -0,0 +1,30 @@
+import { CheckoutService } from "../../../../core/applications/services/checkoutService"
+import { IFaturaRepository } from "../../../../core/ports/IFaturaRepository"
+import { IMeioDePagamento } from "../../../../core/ports/IMeioDePagamento"
+
+interface SutTypes {
+  sut: CheckoutService
+  meioDePagamento: IMeioDePagamento
+  faturaRepository: IFaturaRepository
+}
+
+const makeSut = (): SutTypes => {
+  return {} as SutTypes
+}
+
+describe('CheckoutService', () => {
+  describe('Contructor', () => {
+    it('Espero receber erro na contrução do serviço por não enviar os argumentos necessarios', () => {
+      // @ts-expect-error
+      const caller = () => new CheckoutService()
+      expect(caller).toThrowError(new Error("meioDePagamento e faturaRepository são requeridos!"))
+    })
+  })
+
+  describe('Checkout', () => {
+    it('Espero concluir o checkou com sucesso', async () => {
+      const { sut, meioDePagamento, faturaRepository } = makeSut()
+      // TODO: implementar testes de checkout
+    })
+  })
+})
