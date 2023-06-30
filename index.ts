aa215dde15ef078b8f7c14a6422dc836d53091f9
Lia Dias <liadias914@gmail.com>
2023-06-29 23:10:01 -0300

    (ci) dependencies

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { App } from './src/app';
 const PORT = process.env.PORT || 3000;
-import { router } from "./src/config/express"
+import { router } from "./src/config/express/express"
 
 new App(router).listen(PORT);
