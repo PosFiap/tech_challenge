ba563c81299522fc49f5f39b8a2c309a7b92b86b
Sandro <misfitssandro@gmail.com>
2023-07-05 13:46:28 -0300

    feat: atualizad status do pedido devido a pagamento efetuado no httpMock

--- src/adapter/controller/PagamentoQrCodeAdapter.ts
+++ src/adapter/controller/PagamentoQrCodeAdapter.ts
@@ -1,9 +1,12 @@
 import { CheckoutService } from '../../modules/pagamento'
+import { PedidoService } from '../../modules/pedido/PedidoService'
 import { Fatura, MeioPagamentoMercadoPago } from '../gateways/MeioPagamentoMercadoPago'
 import { HttpClientMock } from '../infra/HttpsMock'
 
 export class PagamentoQrCodeAdapter {
-  async gerarPagamentoQrCode(pedido: Fatura): Promise<string> {
+  async gerarPagamentoQrCode(pedidoId: string): Promise<string> {
+    const pedidoService = new PedidoService()
+    const pedido = {} as Fatura
     const qrCode = new MeioPagamentoMercadoPago(new HttpClientMock())
     const checkoutService = new CheckoutService(qrCode)
     const result = await checkoutService.checkoutQrCode(pedido)
--- src/adapter/infra/HttpsMock.ts
+++ src/adapter/infra/HttpsMock.ts
@@ -1,7 +1,15 @@
 import { HttpRequest, HttpResponse, IHttp } from "../../modules/pagamento/ports/IHttp";
+import { PedidoAdapter } from "../controller/PedidoAdapter";
+import { AtualizaStatusPedidoDTO } from "./../../modules/pedido"
 
 export class HttpClientMock implements IHttp {
   async request<T>(_config: HttpRequest): Promise<HttpResponse<T>> {
+    const pedidoAdapter = new PedidoAdapter()
+    const pedidoStatusDTO = new AtualizaStatusPedidoDTO(
+      _config.body.external_reference,
+      1
+    )
+    await pedidoAdapter.atualizaStatusPedido(pedidoStatusDTO)
     return { statusCode: 200, body: { qr_data: 'valor para gerar QRCode' } as T }
   }
 }
