53b3a206afdc32db3244d14611dc8d7e40dda140
Sandro <misfitssandro@gmail.com>
2023-07-06 03:59:55 -0300

    feat: implementado http client mock

--- src/adapter/infra/HttpsMock.ts
+++ src/adapter/infra/HttpsMock.ts
@@ -1,15 +1,11 @@
-import { HttpRequest, HttpResponse, IHttp } from "../../modules/pagamento/ports/IHttp";
-import { PedidoAdapter } from "../controller/PedidoAdapter";
-import { AtualizaStatusPedidoDTO } from "./../../modules/pedido"
+import { EStatus, HttpRequest, HttpResponse, IHttp } from '../../modules/pagamento'
+import { PagamentoPedidoRepository } from '../persistence/PagamendoPedidoRepository'
 
 export class HttpClientMock implements IHttp {
   async request<T>(_config: HttpRequest): Promise<HttpResponse<T>> {
-    const pedidoAdapter = new PedidoAdapter()
-    const pedidoStatusDTO = new AtualizaStatusPedidoDTO(
-      _config.body.external_reference,
-      1
-    )
-    await pedidoAdapter.atualizaStatusPedido(pedidoStatusDTO)
-    return { statusCode: 200, body: { qr_data: 'valor para gerar QRCode' } as T }
+    const pedidoRepository = new PagamentoPedidoRepository()
+    await pedidoRepository.atualizarStatusPedidoPago(parseInt(_config.body.external_reference, 10), EStatus['Pedido em preparação'])
+    const result = { qr_data: 'valor para gerar QRCode' }
+    return { statusCode: 200, body: result as T }
   }
 }
