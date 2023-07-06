71834d08c048434c0f10975c751a91fa3771e033
Sandro <misfitssandro@gmail.com>
2023-07-06 04:00:21 -0300

    feat: implementado pagamento pedido repository

---
+++ src/adapter/persistence/PagamendoPedidoRepository.ts
@@ -0,0 +1,20 @@
+import { EStatus, PedidoPagamentoDTO } from '../../modules/pagamento/dto/PedidoPagamentoDTO'
+import { IPagamentoPedidoRegistry } from '../../modules/pagamento/ports/IPedidoRegistry'
+
+const bancoDeDados: Array<Record<string, any>> = []
+
+export class PagamentoPedidoRepository implements IPagamentoPedidoRegistry {
+  async atualizarStatusPedidoPago (codigo: number, status: EStatus): Promise<boolean> {
+    bancoDeDados[codigo].status = status
+    return true
+  }
+
+  async obterPedidoPeloCodigo (codigo: number): Promise<PedidoPagamentoDTO> {
+    const pedido = bancoDeDados[codigo]
+    return {
+      codigo: pedido.codigo,
+      status: pedido.status,
+      itensDePedido: pedido.itensDePedido
+    }
+  }
+}
