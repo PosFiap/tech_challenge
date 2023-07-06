25367dc5c665555fc1ab0da7af60f53cb0d779d4
Sandro <misfitssandro@gmail.com>
2023-07-06 03:57:19 -0300

    test: refactorado teste do checkout service

--- src/modules/pagamento/__tests__/checkoutService.test.ts
+++ src/modules/pagamento/__tests__/checkoutService.test.ts
@@ -1,42 +1,60 @@
-import { CheckoutService } from "../checkoutService"
-import { IMeioDePagamentoQR } from "../ports/IMeioDePagamentoQR"
+import { CheckoutService } from '../checkoutService'
+import { PedidoPagamentoDTO, EStatus } from '../dto'
+import { IMeioDePagamentoQR, IPagamentoPedidoRegistry } from '../ports'
 
 interface SutTypes {
-  sut: CheckoutService<any, any>
+  sut: CheckoutService<any>
   meioDePagamento: IMeioDePagamentoQR<any, any>
+  pagamentoPedidoRepository: IPagamentoPedidoRegistry
 }
 
-class meioDePagamentoMock implements IMeioDePagamentoQR<any, any> {
-  async checkoutQrCode(_pedido: any): Promise<any> {
+class MeioDePagamentoMock implements IMeioDePagamentoQR<any, any> {
+  async checkoutQrCode (_pedido: any): Promise<any> {
     return true
   }
 }
 
+class PedidoPagamentoRepository implements IPagamentoPedidoRegistry {
+  async obterPedidoPeloCodigo (codigo: number): Promise<PedidoPagamentoDTO> {
+    throw new Error('Method not implemented.')
+  }
+
+  async atualizarStatusPedidoPago (codigo: number, status: EStatus): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+}
+
 const makeSut = (): SutTypes => {
-  const meioDePagamento = new meioDePagamentoMock()
-  const sut = new CheckoutService(meioDePagamento)
+  const meioDePagamento = new MeioDePagamentoMock()
+  const pagamentoPedidoRepository = new PedidoPagamentoRepository()
+  const sut = new CheckoutService(meioDePagamento, pagamentoPedidoRepository)
   return {
     sut,
-    meioDePagamento
+    meioDePagamento,
+    pagamentoPedidoRepository
   }
 }
 
 describe('CheckoutService', () => {
   describe('Contructor', () => {
     it('Espero receber erro na contrução do serviço por não enviar os argumentos necessarios', () => {
-      // @ts-expect-error
-      const caller1 = () => new CheckoutService()
-      expect(caller1).toThrowError(new Error("meioDePagamento é requerido."))
+      // @ts-expect-error test error in constructor
+      const caller1 = (): CheckoutService => new CheckoutService()
+      expect(caller1).toThrowError(new Error('meioDePagamento é requerido.'))
     })
   })
 
   describe('Checkout', () => {
     it('Espero concluir o checkou com sucesso', async () => {
-      const { sut, meioDePagamento } = makeSut()
+      const { sut, meioDePagamento, pagamentoPedidoRepository } = makeSut()
       const meioDePagamentoSpy = jest.spyOn(meioDePagamento, 'checkoutQrCode')
-      const res = await sut.checkoutQrCode({ any: 'any value' })
-      expect(res).toBe(true)
-      expect(meioDePagamentoSpy).toBeCalledWith({ any: 'any value' })
+      const pedidoSpy = jest.spyOn(pagamentoPedidoRepository, 'obterPedidoPeloCodigo')
+        .mockResolvedValueOnce({ codigo: 0, status: 0, itensDePedido: [] })
+      const res = await sut.checkoutQrCode(1)
+      expect(res.sucesso).toBe(true)
+      expect(pedidoSpy).toBeCalledTimes(1)
+      expect(pedidoSpy).toBeCalledWith(1)
+      expect(meioDePagamentoSpy).toBeCalledWith({ codigo: 0, status: 0, itensDePedido: [] })
       expect(meioDePagamentoSpy).toBeCalledTimes(1)
     })
   })
