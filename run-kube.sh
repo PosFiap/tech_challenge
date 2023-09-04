0b874fa14150282202f679dfd2ff4ef95b4c471f
Mateus Wensch <wensch22@gmail.com>
2023-09-04 18:49:14 -0300

    Create Secrets and Config LoadBalancer

--- run-kube.sh
+++ run-kube.sh
@@ -5,19 +5,28 @@ option=$1
 case $option in
   db) 
     kubectl create namespace postgres
+    kubectl apply -f ./kubernetes/configmap/db-deployment-configmap.yaml
+    kubectl apply -f ./kubernetes/secrets/postgres-secrets.yaml
     kubectl apply -f ./kubernetes/deployments/db-deployment.yaml
     kubectl apply -f ./kubernetes/services/db-service.yaml
+    kubectl apply -f ./kubernetes/volume.yaml
     ;;
   app)
     kubectl create namespace challenge
+    kubectl apply -f ./kubernetes/configmap/deployment-configmap.yaml
+    kubectl apply -f ./kubernetes/secrets/challenge-secrets.yaml
     kubectl apply -f ./kubernetes/deployments/deployment.yaml
     kubectl apply -f ./kubernetes/services/service.yaml
     ;;
   all) 
     kubectl create namespace postgres
     kubectl create namespace challenge
+    kubectl apply -f ./kubernetes/configmap/db-deployment-configmap.yaml
+    kubectl apply -f ./kubernetes/secrets/postgres-secrets.yaml
     kubectl apply -f ./kubernetes/deployments/db-deployment.yaml
     kubectl apply -f ./kubernetes/services/db-service.yaml
+    kubectl apply -f ./kubernetes/configmap/deployment-configmap.yaml
+    kubectl apply -f ./kubernetes/secrets/challenge-secrets.yaml
     kubectl apply -f ./kubernetes/deployments/deployment.yaml
     kubectl apply -f ./kubernetes/services/service.yaml
     ;;
