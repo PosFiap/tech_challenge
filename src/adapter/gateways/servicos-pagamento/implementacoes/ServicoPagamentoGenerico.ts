7c8bf097cdc77a92394c81b6b39620dcbe267e0f
Lia Dias <liadias914@gmail.com>
2023-09-04 23:15:46 -0300

    Add sufixo em fatura pagamento

--- src/adapter/gateways/servicos-pagamento/implementacoes/ServicoPagamentoGenerico.ts
+++ src/adapter/gateways/servicos-pagamento/implementacoes/ServicoPagamentoGenerico.ts
@@ -2,7 +2,7 @@ import { IServicoPagamentoGateway } from '../interfaces/IServicoPagamentoGateway
 
 export class ServicoPagamentoGatewayGenerico implements IServicoPagamentoGateway {
   async obtemFaturaPagamento (valor: number): Promise<string> {
-    return Math.round(Math.random() * 9999999999).toString().padEnd(10, '0')
+    return `PAG-${Math.round(Math.random() * 9999999999).toString().padEnd(10, '0')}`;
   }
 }
 
