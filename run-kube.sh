ba5a73f7520870b34a6e3045a8e827eae5108f29
Lucas Bortolo <lucasbortolo3@gmail.com>
2023-09-04 21:58:54 -0300

    Ajustes kubernetes run

--- run-kube.sh
+++ run-kube.sh
@@ -4,39 +4,37 @@ option=$1
 
 case $option in
   db) 
-    kubectl create namespace postgres
     kubectl apply -f ./kubernetes/configmap/db-deployment-configmap.yaml
     kubectl apply -f ./kubernetes/secrets/postgres-secrets.yaml
+    kubectl apply -f ./kubernetes/volume.yaml
     kubectl apply -f ./kubernetes/deployments/db-deployment.yaml
     kubectl apply -f ./kubernetes/services/db-service.yaml
-    kubectl apply -f ./kubernetes/volume.yaml
+    
     ;;
   app)
-    kubectl create namespace challenge
     kubectl apply -f ./kubernetes/configmap/deployment-configmap.yaml
     kubectl apply -f ./kubernetes/secrets/challenge-secrets.yaml
     kubectl apply -f ./kubernetes/deployments/deployment.yaml
     kubectl apply -f ./kubernetes/services/service.yaml
     ;;
   all) 
-    kubectl create namespace postgres
-    kubectl create namespace challenge
     kubectl apply -f ./kubernetes/configmap/db-deployment-configmap.yaml
     kubectl apply -f ./kubernetes/secrets/postgres-secrets.yaml
+    kubectl apply -f ./kubernetes/volume.yaml
     kubectl apply -f ./kubernetes/deployments/db-deployment.yaml
     kubectl apply -f ./kubernetes/services/db-service.yaml
+
     kubectl apply -f ./kubernetes/configmap/deployment-configmap.yaml
     kubectl apply -f ./kubernetes/secrets/challenge-secrets.yaml
     kubectl apply -f ./kubernetes/deployments/deployment.yaml
     kubectl apply -f ./kubernetes/services/service.yaml
     ;;
-  clear)  
-    kubectl delete deployments --all -n postgres
-    kubectl delete services --all -n postgres
-    kubectl delete pods --all -n postgres
-    kubectl delete deployments --all -n challenge
-    kubectl delete services --all -n challenge
-    kubectl delete pods --all -n challenge
+  clear)
+    kubectl delete --all configmaps
+    kubectl delete --all secrets
+    kubectl delete --all deployments
+    kubectl delete --all svc
+    kubectl delete --all pods
     ;;
   *) echo 'Comando não encontrado!' ;;
 esac
